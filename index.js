ae9ee308a06aaf116310c2f2b07798b283d0e103
Aleksei Treshchalin <treshchal@gmail.com>
2023-01-28 20:06:14 +0300

    Add resolution change

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let mouseIsDown = false;
 
 const boardScreen = document.querySelector("#board-screen");
 const clearButton = document.querySelector("#clear-button");
+const sizeRadioButtons = document.querySelectorAll(".radio-size-button");
 const toggleGridButton = document.querySelector("#grid-button");
 
 // Watch mouse button status
@@ -15,29 +16,35 @@ document.addEventListener("mouseup", () => {
 });
 
 // Fill screen with 'pixels'
-for (let count = 0; count < 48; count++) {
-  const screenPixel = document.createElement("div");
-  screenPixel.classList.add("screen-pixel");
-  if (count % 8 === 0) screenPixel.classList.add("screen-pixel_first-column");
-  if (count < 8) screenPixel.classList.add("screen-pixel_first-row");
-  boardScreen.appendChild(screenPixel);
-}
+const fillScreen = (size) => {
+  boardScreen.style.gridTemplateColumns = `repeat(${size * 4}, auto)`;
+  const totalPixels = 12 * Math.pow(size, 2);
+  for (let count = 0; count < totalPixels; count++) {
+    const screenPixel = document.createElement("div");
+    screenPixel.classList.add("screen-pixel");
+    if (count % (size * 4) === 0)
+      screenPixel.classList.add("screen-pixel_first-column");
+    if (count < size * 4) screenPixel.classList.add("screen-pixel_first-row");
+    boardScreen.appendChild(screenPixel);
+  }
+};
 
 // Color change for initial mousedown
 boardScreen.addEventListener("mousedown", (event) => {
-  event.target.style.backgroundColor = "#808080";
+  if (event.target.classList.contains("screen-pixel"))
+    event.target.style.backgroundColor = "#808080";
 });
 
 // Color change for subsequent mouse movement
 boardScreen.addEventListener("mouseover", (event) => {
-  if (mouseIsDown && event.target !== boardScreen)
+  if (mouseIsDown && event.target.classList.contains("screen-pixel"))
     event.target.style.backgroundColor = "#808080";
 });
 
 // Clear button
 clearButton.addEventListener("click", () => {
   document.querySelectorAll(".screen-pixel").forEach((pixel) => {
-    pixel.style.backgroundColor = "#f0f0f0";
+    pixel.style.backgroundColor = "transparent";
   });
 });
 
@@ -49,3 +56,15 @@ toggleGridButton.addEventListener("click", () => {
     boardScreen.classList.add("board-screen_grid");
   }
 });
+
+// Change screen resolution
+sizeRadioButtons.forEach((button) =>
+  button.addEventListener("change", (event) => {
+    while (boardScreen.firstChild) {
+      boardScreen.removeChild(boardScreen.lastChild);
+    }
+    fillScreen(event.target.dataset.size);
+  })
+);
+
+fillScreen(10);
